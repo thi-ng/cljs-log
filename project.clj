5033e6485f6d617abde4a14b79d82369910ce714
Karsten Schmidt <k@postspectacular.com>
2016-07-05 08:41:17 +0100

    fix #1, add :jar-exclusions, update deps, 0.2.3 release

--- project.clj
+++ project.clj
@@ -1,16 +1,18 @@
-(defproject cljs-log "0.2.2"
+(defproject cljs-log "0.2.3"
   :description  "Logging macros for Clojurescript"
   :url          "https://github.com/postspectacular/cljs-log"
   :license      {:name "Apache Software License 2.0"
                  :url "http://www.apache.org/licenses/LICENSE-2.0"}
 
-  :dependencies [[environ "1.0.0"]]
+  :dependencies [[environ "1.0.3"]]
 
-  :profiles     {:provided {:dependencies [[org.clojure/clojure "1.7.0"]
-                                           [org.clojure/clojurescript "1.7.122"]]}
+  :jar-exclusions [#"^public/"]
 
-                 :dev      {:plugins      [[lein-cljsbuild "1.0.6"]
-                                           [lein-environ "1.0.0"]]
+  :profiles     {:provided {:dependencies [[org.clojure/clojure "1.8.0"]
+                                           [org.clojure/clojurescript "1.9.89"]]}
+
+                 :dev      {:plugins      [[lein-cljsbuild "1.1.3"]
+                                           [lein-environ "1.0.3"]]
                             :cljsbuild    {:builds
                                            [{:id "dev"
                                              :source-paths ["src" "test"]
